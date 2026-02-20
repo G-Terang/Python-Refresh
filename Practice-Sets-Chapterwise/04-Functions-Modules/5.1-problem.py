4ed56156daf2becb09e300d1c2ef14d88dac9338
G Terang <ganeshisthebossofallpeople@gmail.com>
2026-02-20 11:21:43 +0530

    Updated Problem 5.1 Solution

--- Practice-Sets-Chapterwise/04-Functions-Modules/5.1-problem.py
+++ Practice-Sets-Chapterwise/04-Functions-Modules/5.1-problem.py
@@ -7,3 +7,11 @@ Q5. Modules and Pip – Using External Libraries
     2.  Install and import the requests module (if available) and use it to fetch data from "https://api.github.com" .
 
 '''
+
+import math
+
+# 1. square root of 144
+print(math.sqrt(144))  # 12.0
+
+# 2. sin(90°)
+print(math.sin(math.radians(90)))  # 1.0
